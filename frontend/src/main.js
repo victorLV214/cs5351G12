79ba7cb19a18becf1a01c9d603467c04ba8f8e9b
MioAyanokouji <chencwang4-c@my.cityu.edu.hk>
2024-11-24 04:05:37 +0800

    user profile

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,6 +7,8 @@ import 'element-plus/dist/index.css'
 import locale from 'element-plus/es/locale/lang/zh-cn'
 
 import '@/assets/styles/index.scss' // global css
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 
 import App from './App'
 import store from './store'
