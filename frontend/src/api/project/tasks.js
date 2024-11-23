7bad8d6de97c45e13f4d2a5ab7b8c5b6b0b02cb6
LYU mingyuan <victorlv0312@gmail.com>
2024-11-23 13:08:48 +0800

    tasks list

---
+++ frontend/src/api/project/tasks.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+
+// 查询任务列表
+export function listTask(query) {
+    return request({
+        url: '/dev-api/project/task/list',
+        method: 'get',
+        params: query
+    })
+}
+
+// 新增任务
+export function addTask(data) {
+    return request({
+        url: '/dev-api/project/task',
+        method: 'post',
+        data: data
+    })
+}
+
+// 修改任务
+export function updateTask(data) {
+    return request({
+        url: '/dev-api/project/task',
+        method: 'put',
+        data: data
+    })
+}
+
+// 获取任务详细信息
+export function getTask(taskId) {
+    return request({
+        url: '/dev-api/project/task/' + taskId,
+        method: 'get'
+    })
+}
+
+// 删除任务
+export function delTask(taskId) {
+    return request({
+        url: '/dev-api/project/task/' + taskId,
+        method: 'delete'
+    })
+}
--- frontend/src/views/project/project/detail.vue
+++ frontend/src/views/project/project/detail.vue
@@ -40,28 +40,28 @@
 
         <el-menu-item index="releases">
           <el-icon><Upload /></el-icon>
-          <span>发布管理</span>
+          <span>任务管理</span>
         </el-menu-item>
 
-        <el-menu-item index="baseline">
-          <el-icon><Files /></el-icon>
-          <span>基线管理</span>
-        </el-menu-item>
+<!--        <el-menu-item index="baseline">-->
+<!--          <el-icon><Files /></el-icon>-->
+<!--          <span>基线管理</span>-->
+<!--        </el-menu-item>-->
 
-        <el-menu-item index="reviews">
-          <el-icon><ChatDotRound /></el-icon>
-          <span>评审管理</span>
-        </el-menu-item>
+<!--        <el-menu-item index="reviews">-->
+<!--          <el-icon><ChatDotRound /></el-icon>-->
+<!--          <span>评审管理</span>-->
+<!--        </el-menu-item>-->
 
-        <el-menu-item index="testing">
-          <el-icon><Histogram /></el-icon>
-          <span>测试管理</span>
-        </el-menu-item>
+<!--        <el-menu-item index="testing">-->
+<!--          <el-icon><Histogram /></el-icon>-->
+<!--          <span>测试管理</span>-->
+<!--        </el-menu-item>-->
 
-        <el-menu-item index="documents">
-          <el-icon><Document /></el-icon>
-          <span>文档管理</span>
-        </el-menu-item>
+<!--        <el-menu-item index="documents">-->
+<!--          <el-icon><Document /></el-icon>-->
+<!--          <span>文档管理</span>-->
+<!--        </el-menu-item>-->
 
         <el-menu-item index="resources">
           <el-icon><Connection /></el-icon>
